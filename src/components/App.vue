1424856b56e1dd927b25b921e694e85e28934ba2
Sergio Glorias <sergioglorias@gmail.com>
2021-06-03 11:35:59 +0100

    WIP

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row py-2 border-bottom bg-info text-white px-5">
+    <div class="col-6 d-flex align-items-center">
+      <img
+        src="./assets/slaycer.png"
+        width="70"
+        height="70"
+        class="rounded-circle"
+      />
+      <h1 class="title fs-1 mx-2">Slaycer</h1>
+    </div>
+    <div class="col-6"></div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
+.title {
+  font-family: "Lobster", cursive;
+}
+</style>
